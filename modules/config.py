b291762e4ab0451cd23f279f3244a72c4f319c64
king8879 <k4507244@gmail.com>
2025-04-26 13:46:24 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
-BOT_NAME = os.environ.get('BOT_NAME')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-API_ID = int(os.environ.get('API_ID'))
-API_HASH = os.environ.get('API_HASH')
-MONGO_URI = os.environ.get('MONGO_URI')
+BOT_NAME = os.environ.get('@uploader8879_bot')
+BOT_TOKEN = os.environ.get('8153797500:AAFDsNv_OWSYEU4lSbRBR5lsgr5UDaHYlgs')
+API_ID = int(os.environ.get('22923037'))
+API_HASH = os.environ.get('dfb3666878b3851460a58461c5a50f5b')
+MONGO_URI = os.environ.get('mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority')
