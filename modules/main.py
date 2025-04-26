73871583fd528b74006f7a16850c3a32cda804e1
king8879 <k4507244@gmail.com>
2025-04-26 13:53:03 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -40,11 +40,11 @@ bot = Client("bot",
 # Get the MongoDB collection for this bot
 collection = get_collection(BOT_NAME, MONGO_URI)
 # Constants
-OWNER_IDS = [7448837918]  # Replace with the actual owner user IDs
+OWNER_IDS = [6554343173]  # Replace with the actual owner user IDs
 
 # Global variables
-log_channel_id = 7448837918
-authorized_users = [7448837918]
+log_channel_id = -1002540145139
+authorized_users = [6554343173]
 ALLOWED_CHANNEL_IDS = []
 my_name = "**𝚉𝙴𝙽𝙸𝚃𝙷 🏅**"
 overlay = None 
